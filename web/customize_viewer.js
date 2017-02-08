f29de081cf9f991cc8c8022f0dee271d8251a3b7
Daniel Johnson <danielj@ifixit.com>
2017-02-07 16:00:34 -0800

    show page number without an <input>
    Display the page number in a span, since we
    don't want the user to be able to switch
    pages using the input box.
    
    There is some js relying on the input box
    existing, so just hide it for now. I think
    it's simpler to try not to edit the forked
    files too much. The viewer is pretty close to
    what we want as-is.

--- web/customize_viewer.js
+++ web/customize_viewer.js
@@ -7,14 +7,19 @@
 
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define('pdfjs-web/customize_viewer', ['exports'], factory);
+    define('pdfjs-web/customize_viewer', ['exports', 'pdfjs-web/dom_events'],
+     factory);
   } else if (typeof exports !== 'undefined') {
-    factory(exports);
+    factory(exports, require('./dom_events.js'));
   } else {
-    factory((root.pdfjsWebCustomizeViewer = {}));
+    factory((root.pdfjsWebCustomizeViewer = {}), root.pdfjsWebDOMEvents);
   }
-}(this, function (exports) {
+}(this, function (exports, DOMEvents) {
+  var eventBus = DOMEvents.getGlobalEventBus();
 
-console.log('test');
+  eventBus.on('pagechange', function(ev) {
+    var pageNumberDisplay = document.getElementById('pageNumberDisplay');
+    pageNumberDisplay.innerText = ev.pageNumber;
+  });
 
 }));
